db283c1b497213eae68c23d31ee643c3bc3e96cc
KimMinchan95 <hialscks@gmail.com>
2021-11-06 20:18:34 +0900

    update: lecture 3

--- index.ts
+++ index.ts
@@ -1,1 +1,11 @@
-console.log("hello");
+const name = 'Chan',
+  age = 27,
+  gender = 'male';
+
+const sayHi = (name, age, gender?) => {
+    console.log(`Hello ${name}, you are ${age}, you are s ${gender}`);
+}
+  
+sayHi(name, age);
+
+export { };
