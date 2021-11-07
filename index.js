b65fad4cf2849d03e3b2b1a87bddae9e6fe90737
KimMinchan95 <hialscks@gmail.com>
2021-11-07 12:57:02 +0900

    update: lecture 6

--- index.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const name = 'Chan', age = 27, gender = 'male';
-const sayHi = (name, age, gender) => {
-    console.log(`Hello ${name}, you are ${age}, you are s ${gender}`);
-};
-sayHi(name, age);
-//# sourceMappingURL=index.js.map
